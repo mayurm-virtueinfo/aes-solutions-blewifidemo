a63f9dac087582a3800b32d7e79d4f60cd68609d
Mac <mac@Macs-MacBook-Pro.local>
2025-07-01 12:31:25 +0530

    - renamed app again for AER Solutions LLC

--- android/app/src/main/java/com/aersolutions/llc/MainActivity.kt
+++ android/app/src/main/java/com/aersolutions/llc/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.aersolutions.app
+package com.aersolutions.llc
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/aersolutions/llc/MainApplication.kt
+++ android/app/src/main/java/com/aersolutions/llc/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.aersolutions.app
+package com.aersolutions.llc
 
 import android.app.Application
 import com.facebook.react.PackageList
